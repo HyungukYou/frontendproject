e84842fc47015b48e81f58f1e308e7f1c9a8d9f1
hyunguk <hyunguk119@gmail.com>
2021-10-07 22:44:33 +0900

    Add new content

--- README.md
+++ README.md
@@ -22,3 +22,23 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 공부 과정
+```
+유튜브 애플코딩 Vue 강의를 보고 기본 문법 배우기
+직접 프로젝트를 만들어서 실력 키우기
+```
+### Vue 1강 주요 내용
+```
+Vue 3버전 설치 및 셋팅
+```
+### Vue 2강 주요 내용
+```
+데이터바인딩 문법 공부
+```
+### Vue 3강 주요 내용
+```
+v-for 반복문 문법 공부
+```
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <div class="menu">
+      <a v-for="a in 메뉴들" :key="a"> {{ a }}</a>
+    </div>
+
+    <div>
+      {{logo}}
+      <h4>{{ products[0] }}</h4>
+      <p>{{ price1 }} 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[1] }}</h4>
+      <p>{{ price2 }} 만원</p>
+    </div>
+    <div>
+      <h4>{{ products[2]}}</h4>
+      <p>{{ price3 }} 만원</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  var array = [10,20,30];
+  array[0]
 
 export default {
   name: 'App',
+  data(){ // 데이터 보관
+    return{
+      price1 : 60, // 자료 이름 , 자료 내
+      price2 : 70,
+      price3 : 80,
+      logo : '원룸샵',
+      스타일 : 'color :blue',
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      메뉴들 : ['Home','Shop','About'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -23,6 +50,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
