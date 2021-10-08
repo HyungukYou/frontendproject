5542679da00a06bd1d5f18744f07dd12c9f25ffb
hyunguk <hyunguk119@gmail.com>
2021-10-08 22:39:39 +0900

    Add new content

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
 
+    <div class="black-bg" v-if="모달창열렸니 == true">
+      <div class="white-bg">
+        <h4>상세페이지임</h4>
+        <p>상세페이지 내용임</p>
+        <button @click="모달창열렸니 = false">닫기</button>
+      </div>
+    </div>
+
     <div class="menu">
       <a v-for="a in 메뉴들" :key="a"> {{ a }}</a>
     </div>
 
+
+
     <div>
       {{logo}}
-      <h4>{{ products[0] }}</h4>
+      <img src="./assets/room0.jpg" class="room-img">
+      <h4 @click="모달창열렸니 == true">{{ products[0] }}</h4>
       <p>{{ price1 }} 만원</p>
       <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
     </div>
     <div>
+      <img src="./assets/room1.jpg" class="room-img">
       <h4>{{ products[1] }}</h4>
       <p>{{ price2 }} 만원</p>
       <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
     </div>
     <div>
+      <img src="./assets/room2.jpg" class="room-img">
       <h4>{{ products[2]}}</h4>
       <p>{{ price3 }} 만원</p>
       <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
@@ -32,6 +45,7 @@ export default {
   name: 'App',
   data(){ // 데이터 보관
     return{
+      모달창열렸니 : true,
       신고수 : [0,0,0],
       price1 : 60, // 자료 이름 , 자료 내
       price2 : 70,
@@ -51,6 +65,26 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
