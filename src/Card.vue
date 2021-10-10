87896460a826c952d49e243d4f41f9cfc81f54d0
hyunguk <hyunguk119@gmail.com>
2021-10-10 14:18:13 +0900

    Add new content

--- src/Card.vue
+++ src/Card.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       <img :src="원룸.image" class="room-img">
-      <h4>{{원룸.title}}</h4>
+      <h4 @click="$emit('openModel', 원룸.id)">{{원룸.title}}</h4>
       <p>{{원룸.price }} 만원</p>
     </div>
   </div>
@@ -13,6 +13,11 @@ export default {
   name: 'Card',
   props : {
     원룸 : Object,
+  },
+  methods : {
+    함수(){
+
+    }
   }
 }
 </script>
